766864d67d9e90c595f25d8883de8517f737f52d
Shiwani <97833337+Shiwani-sahu@users.noreply.github.com>
2022-08-28 01:52:46 +0530

    Love score calculator

---
+++ love.js
@@ -0,0 +1,5 @@
+var yourName=prompt("What is your name");
+var loverName=prompt("What is their name");
+var loveScore=Math.random()*100;
+loveScore=Math.floor(loveScore)+1;
+alert("Love score of "+yourName+" and "+loverName+" is "+loveScore+"%");
